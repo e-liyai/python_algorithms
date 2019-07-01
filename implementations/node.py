a01cc7b4742a5284cdc7b7bda08df2ace102e2b8
Andela-eugene <eugene.liyai@andela.com>
2019-07-01 15:58:10 +0300

    feat: add helper functions

--- implementations/node.py
+++ implementations/node.py
@@ -3,3 +3,9 @@ class Node:
     def __init__(self, value, next_node=None):
         self.value = value
         self.next = next_node
+
+    def add_node(self, node):
+        self.next = node
+
+    def remove_next_node(self):
+        self.next = None
