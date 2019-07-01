1dec1845b1fdbf3aedde5b67af16fc21ea9f64f6
Andela-eugene <eugene.liyai@andela.com>
2019-07-01 21:30:09 +0300

    feat: add graph test

---
+++ tests/test_create_graph.py
@@ -0,0 +1,30 @@
+from implementations.graph import Graph
+graph = Graph()
+
+
+def check_word(source, target):
+    counter, i = 0, 0
+    while i < len(source) and i < len(target):
+        if source[i] is not target[i]:
+            counter += 1
+        i += 1
+    return counter
+
+
+def build_vertice_in_graph(source, words):
+    arr = []
+    for word in words:
+        diff = check_word(source, word)
+        if diff is 1:
+            arr.append(word)
+
+    graph.add_vertice(source, arr)
+
+
+words = ["bit", "but", "put", "big", "pot", "pog", "dog", "lot"]
+
+if __name__ == "__main__":
+    for word in words:
+        build_vertice_in_graph(word, words)
+
+    print(graph.linkedListDict)
