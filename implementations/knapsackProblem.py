54afb6e3a4ca4460ed8e9eeb0a0b5daf0127ebb1
Andela-eugene <eugene.liyai@andela.com>
2019-07-18 17:32:48 +0300

    feat: add knapsack

---
+++ implementations/knapsackProblem.py
@@ -0,0 +1,34 @@
+def knapsack_dynamic_solution(objects, max_weight):
+    # object should be in the form of:
+    #       {
+    #           'weight': [0, 5, 6, 2, 7, 1]
+    #           'value':  [0, 4, 1, 5, 7, 8]
+    #       }
+
+    if not isinstance(objects, dict):
+        return 'Object should be a dict'
+    elif len(objects['weight']) != len(objects['value']):
+        return 'weight value pair should be of same length'
+
+    no_items = 0
+    weight = 0
+    two_dim_array = [[0 for i in range(len(objects['weight']))] for x in range(len(objects['weight']))]
+
+    while no_items <= len(objects['weight']):
+
+        while weight <= max_weight:
+
+            if no_items == 0 or weight == 0:
+                two_dim_array[no_items, weight] = 0
+            elif objects['weight'][no_items] <= weight:
+                two_dim_array[no_items, weight] = max(
+                    objects['value'][no_items] + two_dim_array[no_items - 1][weight - objects['weight'][no_items]],
+                    two_dim_array[no_items - 1][weight]
+                )
+            else:
+                two_dim_array[no_items][weight] = two_dim_array[no_items - 1][weight]
+
+            weight += 1
+        no_items += 1
+
+    print(two_dim_array[len(objects['weight']][max_weight])
