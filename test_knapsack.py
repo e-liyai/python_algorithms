57ea08b36c96c11783c0c0ee06f8fc4fc52e132c
Andela-eugene <eugene.liyai@andela.com>
2019-07-18 17:43:11 +0300

    feat: add knapsack test file

--- test_knapsack.py
+++ test_knapsack.py
@@ -1,1 +1,10 @@
-from implementations import knapsackProblem
+from implementations.knapsackProblem import knapsack_dynamic_solution
+
+if __name__ == '__main__':
+    objects = {
+        'weight': [0, 5, 6, 2, 7, 1],
+        'value':  [0, 4, 1, 5, 7, 8]
+    }
+    max_weight = 7
+
+    knapsack_dynamic_solution(objects, max_weight)
